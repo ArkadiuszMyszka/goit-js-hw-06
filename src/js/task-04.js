d53858d2deed90012832553bc9ddad49edca17ba
Arkadiusz Myszka <lokonhinio@gmail.com>
2023-05-17 18:55:02 +0200

    Ad.4 done

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -7,3 +7,33 @@
 /* eslint-disable linebreak-style */
 
 'use strict';
+
+const buttonIncrement = document.querySelector('button[data-action="increment"]');
+const buttonDecrement = document.querySelector('button[data-action="decrement"]');
+const value = document.querySelector('#value');
+// let currentValue = counterValue.textContent;
+let counterValue = 0;
+function increment() {
+    const currentValue = counterValue + 1;
+    counterValue = currentValue;
+    value.textContent = counterValue;
+    console.log(currentValue);
+}
+function decrement() {
+    const currentValue = counterValue - 1;
+    counterValue = currentValue;
+    value.textContent = counterValue;
+    console.log(currentValue);
+}
+buttonIncrement.addEventListener("click", increment);
+buttonDecrement.addEventListener("click", decrement);
+
+
+
+
+
+
+
+console.log(`------------------------------------`);
+console.log(counterValue);
+console.log(buttonIncrement.dataset.action);
