757386584d5ecb169ee02f92046de7635cd6ce81
Arkadiusz Myszka <lokonhinio@gmail.com>
2023-05-18 00:18:28 +0200

    ad.10 done

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -8,15 +8,38 @@
 
 'use strict';
 
+const create = document.querySelector('button[data-create]');
+const destroy = document.querySelector('button[data-destroy]');
+const div = document.querySelector("#controls");
+const number = div.firstElementChild;
 
 
 
-
-// eslint-disable-next-line no-unused-vars
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+function createStart() {
+  const divBox = document.querySelector("#boxes");
+  const input = number.value;
+  console.log(input);
+  let color = "";
+  function getRandomHexColor() {
+  color = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+  }
+  function createBoxes(amount) {
+    for (let i = 0; i < amount; i += 1) {
+      getRandomHexColor();
+      divBox.insertAdjacentHTML("afterbegin", `<div style ="width:${20 + (10 * i)}px; height:${20 + (10 * i)}px; background-color:${color}"></div>`);
+      console.log(i);
+          }
+  }
+  createBoxes(input);
 }
-console.log("-----------------------");
-console.log();
+function destroyBoxes() {
+  const divBox = document.querySelector("#boxes");
+  divBox.remove();
+  div.insertAdjacentHTML("afterend", `<div id="boxes"></div>`);
+}
+// eslint-disable-next-line no-unused-vars
+
+create.addEventListener("click", createStart);
+destroy.addEventListener("click", destroyBoxes);
