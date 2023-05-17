fe487342990e24087458aa3db4f664d6c7b17ce0
Arkadiusz Myszka <lokonhinio@gmail.com>
2023-05-17 21:53:28 +0200

    ad.8 done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -8,6 +8,28 @@
 
 'use strict';
 
-console.log("-----------------------");
+const form = document.querySelector('.login-form');
+const object = {
+    email: "",
+    password: "",
+};
+function handleSubmit(a) {
+    a.preventDefault();
+    const {
+        elements: { email, password },
+    } = a.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        alert("Uzupełnij pola!");
+    } else {
+         object.email = email.value;
+        object.password = password.value;
+        console.log(object);
+        a.currentTarget.reset();
+    }
+}
+form.addEventListener("submit", handleSubmit);
+
+
 // console.log(input.dataset.length);
 // console.log(input.value.length);
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,21 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable quotes */
+/* eslint-disable no-console */
+/* eslint-disable strict */
+/* eslint-disable indent */
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable linebreak-style */
+
+'use strict';
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+console.log("-----------------------");
+console.log();
+// console.log(input.dataset.length);
+// console.log(input.value.length);
